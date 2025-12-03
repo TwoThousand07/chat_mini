d6b3ca06bf1b76c2a872583c8adb63c04d7afaa2
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-12-04 02:50:50 +0500

    Add max_length constraint to Room model fields

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Room(models.Model):
-    name = models.CharField(unique=True)
-    slug = models.SlugField(unique=True)
+    name = models.CharField(unique=True, max_length=255)
+    slug = models.SlugField(unique=True, max_length=255)
 
     def __str__(self):
         return self.name
