c40ebb81bcdb2151c63f34b964230d297fcf4eb4
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-12-05 01:06:38 +0500

    Add RoomListAPIView and configure API URLs for chat application

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Room, Message
+from .serializers import RoomSerializer, MessageSerializer
+
+
+class RoomListAPIView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
